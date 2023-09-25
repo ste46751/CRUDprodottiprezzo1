5fd13bbfbef52a1d23813688b575b160d2e9b365
ste46751 <stefanquarti060@gmail.com>
2023-09-25 09:13:19 +0200

    esercizio crud

---
+++ CRUDprodottiprezzo/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CRUDprodottiprezzo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.lbl_prezzo = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.txt_prezzo = new System.Windows.Forms.TextBox();
+            this.bttn_aggiungi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_nome
+            // 
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(21, 43);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(47, 16);
+            this.lbl_nome.TabIndex = 0;
+            this.lbl_nome.Text = "Nome:";
+            this.lbl_nome.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lbl_nome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_prezzo
+            // 
+            this.lbl_prezzo.AutoSize = true;
+            this.lbl_prezzo.Location = new System.Drawing.Point(21, 106);
+            this.lbl_prezzo.Name = "lbl_prezzo";
+            this.lbl_prezzo.Size = new System.Drawing.Size(51, 16);
+            this.lbl_prezzo.TabIndex = 1;
+            this.lbl_prezzo.Text = "Prezzo:";
+            this.lbl_prezzo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(24, 62);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 22);
+            this.txt_nome.TabIndex = 2;
+            // 
+            // txt_prezzo
+            // 
+            this.txt_prezzo.Location = new System.Drawing.Point(24, 125);
+            this.txt_prezzo.Name = "txt_prezzo";
+            this.txt_prezzo.Size = new System.Drawing.Size(100, 22);
+            this.txt_prezzo.TabIndex = 3;
+            // 
+            // bttn_aggiungi
+            // 
+            this.bttn_aggiungi.Location = new System.Drawing.Point(24, 165);
+            this.bttn_aggiungi.Name = "bttn_aggiungi";
+            this.bttn_aggiungi.Size = new System.Drawing.Size(119, 36);
+            this.bttn_aggiungi.TabIndex = 4;
+            this.bttn_aggiungi.Text = "Aggiungi";
+            this.bttn_aggiungi.UseVisualStyleBackColor = true;
+            this.bttn_aggiungi.Click += new System.EventHandler(this.bttn_aggiungi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bttn_aggiungi);
+            this.Controls.Add(this.txt_prezzo);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.lbl_prezzo);
+            this.Controls.Add(this.lbl_nome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.Label lbl_prezzo;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.TextBox txt_prezzo;
+        private System.Windows.Forms.Button bttn_aggiungi;
+    }
+}
+
---
+++ CRUDprodottiprezzo/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDprodottiprezzo
+{
+    public partial class Form1 : Form
+    {
+        public struct Prodotto
+        {
+            public string Nome;
+            public float Prezzo;
+        }
+        public Prodotto[] p;
+        public int dim = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string Scrivi(Prodotto p)
+        {
+            
+            
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttn_aggiungi_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
